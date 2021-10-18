d4a644067909c46ae3c9f9a9af09f96ffa646060
Gustavo Henrique Barros De Amorim <gamorim@daitan.com>
2021-10-18 15:19:37 -0300

    mobx setup

--- src/features/last/last.tsx
+++ src/features/last/last.tsx
@@ -2,12 +2,10 @@ import Card from "../card/card";
 import CardController from "../card/cardController";
 import React from "react";
 import Result from "../../models/results";
-import { useDispatch } from "react-redux";
 import { StoreContext } from "../../store/mobx";
 
 const Last = () => {
   const store = React.useContext(StoreContext);
-  const dispatch = useDispatch();
   const [result, setResult] = React.useState<Result>({
     sorteio: 0,
     numeros: [],
